4fbc43345d3328fbb26009fc9a5c7e657da5f415
sasidhar525 <40801358+sasidhar525@users.noreply.github.com>
2018-07-07 08:15:35 +0530

    Create consonant.py

---
+++ consonant.py
@@ -0,0 +1,8 @@
+n=raw_input()
+if(n=="a" or n=="e" or n=="i" or n=="o" or n=="u"):
+           print("Vowel")
+elif(n=="b" or n=="c" or  n=="d" or n=="f" or n=="g" or n=="h" or n=="j" or n=="k" or n=="l" or n=="m" or n=="n" or n=="p" or n=="q" or n=="r" or n=="s" or n=="t" or n=="v" or n=="w" or n=="x" or n=="y" or n=="z"):
+	print("Consonant")
+else:
+	print("invalid")
+  
